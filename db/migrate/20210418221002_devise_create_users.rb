ba5ecb1a96f7d358e87b12877f00cc55a55fe3bc
uttsu0021 <uttsu0021@gmail.com>
2021-12-13 18:53:21 +0900

    ビュー追加

--- db/migrate/20210418221002_devise_create_users.rb
+++ db/migrate/20210418221002_devise_create_users.rb
@@ -9,7 +9,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname, null: false
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_to
+      ken
       t.datetime :reset_password_sent_at
 
       ## Rememberable
