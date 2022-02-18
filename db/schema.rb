93de1e71bc78f49b5cbdb0d39edcb097004f5097
uttsu0021 <uttsu0021@gmail.com>
2022-02-18 21:10:19 +0900

    モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_221002) do
+ActiveRecord::Schema.define(version: 2022_02_02_003208) do
+
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "event", null: false
+    t.string "category", null: false
+    t.integer "date", null: false
+    t.integer "prefectures", null: false
+    t.string "venue", null: false
+    t.integer "fee", null: false
+    t.integer "deadline", null: false
+    t.text "explanation", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_04_18_221002) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
