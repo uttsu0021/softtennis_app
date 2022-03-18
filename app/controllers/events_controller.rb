764dc126ecaa5f067c4eb7ff4ac50e9a06a9aa50
uttsu0021 <uttsu0021@gmail.com>
2022-03-18 13:13:38 +0900

    投稿機能作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,9 +7,12 @@ class EventsController < ApplicationController
     @events = Event.new
   end
 
+  def create
+    Event.create(event_params)
+  end
   private
 
   def event_params
-    params.require(:event).permit(:image)
+    params.require(:event).permit(:image,:event,:category,:date,:prefectures,:venue,:fee,:deadline,:explanation,).marge(user_id: current_user.id)
   end
 end
