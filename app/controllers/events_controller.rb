bae2ed3cc3b9517c94fa3a266d0f876fa3d8d616
uttsu0021 <uttsu0021@gmail.com>
2022-03-28 20:52:07 +0900

    エラーメッセージ

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,11 +8,16 @@ class EventsController < ApplicationController
   end
 
   def create
-    Event.create(event_params)
+    @events = Event.create(event_params)
+    if @events.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   private
 
   def event_params
-    params.require(:event).permit(:image,:event,:category,:date,:prefectures,:venue,:fee,:deadline,:explanation,).marge(user_id: current_user.id)
+    params.require(:event).permit(:image,:event,:category,:date,:prefectures,:venue,:fee,:deadline,:explanation,)
   end
 end
