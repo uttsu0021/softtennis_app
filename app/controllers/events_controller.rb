3149d6cb97f9a2d3cc0097ad9042a5eebe34e3c0
uttsu0021 <uttsu0021@gmail.com>
2022-04-22 20:26:23 +0900

    詳細機能作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,26 @@ class EventsController < ApplicationController
   def new
     @events = Event.new
   end
+<<<<<<< Updated upstream
 end
+=======
+
+  def create
+    @events = Event.create(event_params)
+    if @events.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+  private
+
+  def event_params
+    params.require(:event).permit(:image,:event,:category,:date,:prefectures,:venue,:fee,:deadline,:explanation,).merge(user_id: current_user.id)
+  end
+end
+>>>>>>> Stashed changes
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
 class Event < ApplicationRecord
+<<<<<<< Updated upstream
+=======
+  has_one_attached :image
+  belongs_to :user
+  with_options presence: true do
+    validates :event
+    validates :category
+    validates :date
+    validates :prefectures
+    validates :venue
+    validates :fee
+    validates :deadline
+    validates :explanation
+  end
+>>>>>>> Stashed changes
 end
