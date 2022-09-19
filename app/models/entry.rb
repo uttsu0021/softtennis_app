0e488d185ef5ec7b729b0d335087cab57a4429d2
uttsu0021 <uttsu0021@gmail.com>
2022-09-19 18:45:08 +0900

    正規表現

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,12 +1,19 @@
 class Entry < ApplicationRecord
   belongs_to :event
   belongs_to :user
-  with_options presence: true do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'Full-width characters' } do
     validates :last_name
     validates :first_name
+  end
+
+  with_options presence: true,format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
     validates :last_name_kata
     validates :first_name_kata
-    validates :postal_code
+  end
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :phone, format: { with: /\A\d{11}\z/, message: 'number Input only number' }
     validates :address
     validates :user_id
     validates :event_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,presence: true
+         validates :nickname, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
 
          has_many :events
          has_many :entries
